ac82edb1f9d89bf67913259876e392252341723b
behrami <behram1993@gmail.com>
2020-11-02 14:21:08 -0500

    added css for nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
-import './Button.js'
+import './Navbar.css'
 import { Button } from './Button.js'
 
 function Navbar() {
