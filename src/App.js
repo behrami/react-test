f0f50a9efd4917d622d06898c6d68a6acf2996dd
behrami <behram1993@gmail.com>
2020-11-01 20:44:31 -0500

    changed some formating for app.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact />
-      </Switch>
-    </Router>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
     </>
   );
 }
