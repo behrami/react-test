e4e5246419a832fe97d0ff4cbb667c236a38677c
behrami <behram1993@gmail.com>
2020-11-02 13:43:15 -0500

    fixed typo

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,8 +6,8 @@ const STYLES = ['btn--primary', 'btn--outline']
 const SIZES = ['btn--medium', 'btn--large']
 
 export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
-    constCheckButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
-    constCheckButtonSize = SIZES.includes(buttonSize) ? buttonSize :  SIZES[0]
+    const CheckButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+    const CheckButtonSize = SIZES.includes(buttonSize) ? buttonSize :  SIZES[0]
     return (
         <Link to='/sign-up' className='btn-mobile'>
             <button className={'btn ${checkButtonStyle} ${checkButtonSize}'} onClick={onClick} type={type}>
